9f932f92b1cadf82b52bcf84a049f72d7199185c
Bojidar Markov <75314475+boshk0@users.noreply.github.com>
2023-11-18 20:16:55 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+FILE="/onstart.sh"
+
+WALLET="$1"
+
+# Check if th file does not exists
+if [ ! -f "$FILE" ]; then
+    # Start setting up the container
+    apt update; apt install wget nano tmux less xz-utils -y;
+
+    echo "-------------------- PACKGES INSTALLED --------------------"
+
+    wget -o onezerominer.tar.gz https://github.com/OneZeroMiner/onezerominer/releases/download/v1.2.6/onezerominer-linux-1.2.6.tar.gz;
+    tar -xf onezerominer.tar.gz; rm onezerominer.tar.gz;
+
+    echo "-------------------- MINER INSTALLED --------------------"
+
+    WORKER=$(nvidia-smi -q | grep "Serial Number" | awk '{print $4}')
+    echo "cd onezerominer-linux; ./onezerominer -a dynex -o dnx.eu.neuropool.net:19331 -w $WALLET -p $WORKER" > onstart.sh; chmod +x onstart.sh
+
+    echo "-------------------- ONSTART.SH INSTALLED --------------------"
+fi
+
+if ! tmux has-session -t miner 2>/dev/null; then
+    tmux new -d -s miner
+    tmux send-keys -t miner "bash onstart.sh" C-m
+    echo "-------------------- TMUX MINER SESSION STARTED --------------------"
+fi
