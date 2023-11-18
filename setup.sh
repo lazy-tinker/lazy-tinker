39fc88f85728a99755a88c5033ce619dec566b63
Bojidar Markov <75314475+boshk0@users.noreply.github.com>
2023-11-18 22:47:55 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE="~/onstart.sh"
+FILE="/usr/local/bin/onstart.sh"
 
 # Check if th file does not exists
 if [ ! -f "$FILE" ]; then
@@ -8,20 +8,21 @@ if [ ! -f "$FILE" ]; then
     apt update; apt install wget nano tmux less xz-utils systemctl -y;
 
     echo "-------------------- APT PACKGES INSTALLED --------------------"
-
-    cd ~ # Home folder
     
     # Download miner zip file
-    wget https://github.com/OneZeroMiner/onezerominer/releases/download/v1.2.6/onezerominer-linux-1.2.6.tar.gz;
-    tar -xf onezerominer-linux-1.2.6.tar.gz; rm onezerominer-linux-1.2.6.tar.gz;
+    wget -P /usr/local/bin https://github.com/OneZeroMiner/onezerominer/releases/download/v1.2.6/onezerominer-linux-1.2.6.tar.gz;
+    tar -xf /usr/local/bin/onezerominer-linux-1.2.6.tar.gz; rm /usr/local/bin/onezerominer-linux-1.2.6.tar.gz;
 
     echo "-------------------- MINER INSTALLED --------------------"
 
     # Download onstart.sh file
-    wget https://raw.githubusercontent.com/boshk0/HiveOS_GPU_tunner/main/onstart.sh; chmod +x onstart.sh
+    wget -P /usr/local/bin https://raw.githubusercontent.com/boshk0/HiveOS_GPU_tunner/main/onstart.sh; chmod +x /usr/local/bin/onstart.sh
 
     # Download onstart.service file
-    wget -P /etc/systemd/system/ https://raw.githubusercontent.com/boshk0/HiveOS_GPU_tunner/main/onstart.service;
+    wget -P /etc/systemd/system https://raw.githubusercontent.com/boshk0/HiveOS_GPU_tunner/main/onstart.service;
+
+    read -p "Please copy your Dynex wallet: " WALLET
+    sed -i 's|ExecStart=/usr/local/bin/onstart.sh \[WALLET\]|ExecStart=/usr/local/bin/onstart.sh $WALLET|' /etc/systemd/system/onstart.service
 
     # Reload systemd Manager Configuration
     sudo systemctl daemon-reload
