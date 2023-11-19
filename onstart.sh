1acd9faed7b72aeecd044e7f6aa973d41c9a341c
Bojidar Markov <75314475+boshk0@users.noreply.github.com>
2023-11-19 15:30:42 +0200

    Update onstart.sh

--- onstart.sh
+++ onstart.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 WALLET=[WALLET]
-WORKER=$(nvidia-smi -q -i 0 | grep 'Serial Number' | awk '{print $4}')
+
 if ! tmux has-session -t miner 2>/dev/null; then
+    WORKER=$(nvidia-smi -q -i 0 | grep 'Serial Number' | awk '{print $4}')
+
     tmux new -d -s miner
     tmux send-keys -t miner "cd /usr/local/bin/onezerominer-linux; ./onezerominer -a dynex -o dnx.eu.neuropool.net:19331 -w $WALLET -p $WORKER" C-m
 fi
