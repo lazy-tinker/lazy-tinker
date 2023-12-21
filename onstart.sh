c525500af517d4947b4408e1d94f30ef1647b75a
Bojidar Markov <75314475+boshk0@users.noreply.github.com>
2023-12-21 16:08:23 +0200

    Update onstart.sh

--- onstart.sh
+++ onstart.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 WALLET=[WALLET]
+ALGO=[ALGO]
 
 if ! tmux has-session -t miner 2>/dev/null; then
     WORKER=$(nvidia-smi -q -i 0 | grep 'Serial Number' | awk '{print $4}')
 
     tmux new -d -s miner
-    tmux send-keys -t miner "cd /usr/local/bin/onezerominer-linux; ./onezerominer -a dynex -o dnx.eu.neuropool.net:19331 -w $WALLET -p $WORKER" C-m
+
+    if [ "$ALGO" = "dynex" ]; then
+        tmux send-keys -t miner "cd /usr/local/bin/onezerominer-linux; ./onezerominer -a dynex -o dnx.eu.neuropool.net:19331 -w $WALLET -p $WORKER" C-m
+    fi
+    if [ "$ALGO" = "nexa" ]; then
+        tmux send-keys -t miner "cd /usr/local/bin/lolminer; ./lolMiner -a nexa -p stratum+tcp://nexapow.auto.nicehash.com:9200 -u $WALLET.$WORKER" C-m
+    fi
+    
 fi
